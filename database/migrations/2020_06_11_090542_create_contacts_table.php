83f8508e27e73ed2a8e159f688f602aee9e04e54
Tolga <tolgaguler22@gmail.com>
2020-06-11 14:02:45 +0200

    Savedata function made
    made a savedata function in the contact controller. made a model for the contact table. added route to contact page in he pagecontroller

---
+++ database/migrations/2020_06_11_090542_create_contacts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->bigIncrements('id')->autoIncrement();
+            $table->integer('user_id');
+            $table->string('name', 128)->nullable(false);
+            $table->string('email', 128)->nullable(false);
+            $table->text('message')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact');
+    }
+}
