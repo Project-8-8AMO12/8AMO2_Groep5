3e6d59b8b0a6267674556a95824b03d8426f333e
MomandWiar <1045027@mborijnland.nl>
2020-06-21 16:43:13 +0200

    activiteteiten page added

--- resources/views/frontend/activiteiten.blade.php
+++ resources/views/frontend/activiteiten.blade.php
@@ -1,2 +1,85 @@
 @extends('layouts.standard.app')
 
+@section('content')
+
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 190 1440 119">
+        <path fill="#a9ba4f" fill-opacity="1" d="M0,256L288,224L576,256L864,288L1152,224L1440,192L1440,320L1152,320L864,320L576,320L288,320L0,320Z"></path>
+    </svg>
+    <div class="jumbotron jumbotron-fluid text-white d-flex p-0 m-0 imkersGreen">
+        <div class="container my-5">
+            <h1 class="text-center font-weight-bold display-4 mb-5">Cursussen</h1>
+            <div class="row text-center">
+                <p class="h3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+                    id est laborum.
+                </p>
+            </div>
+        </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 180 1440 140">
+        <path fill="#a9ba4f" fill-opacity="1" d="M0,224L360,224L720,256L1080,192L1440,288L1440,0L1080,0L720,0L360,0L0,0Z"></path>
+    </svg>
+
+    <section class="h-50 py-5">
+        <div class="container">
+            <div class="row">
+                <div class="bg-danger rounded col-4 float-right p-5 m-0">
+                    <p class="text-white">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+                        id est laborum.
+                    </p>
+                </div>
+                <div class="col-8 bg-success rounded">
+                    Right
+                </div>
+            </div>
+            <div class="row py-5">
+                <div class="col-8 bg-success rounded">
+                    Left
+                </div>
+                <div class="bg-danger rounded col-4 float-right p-5 m-0">
+                    <p class="text-white">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+                        id est laborum.
+                    </p>
+                </div>
+            </div>
+            <div class="row bg-success">
+                <div class="col-4 bg-success rounded">
+                    left
+                </div>
+                <div class="bg-danger rounded col-4 float-right p-5 m-0">
+                    <p class="text-white">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+                        id est laborum.
+                    </p>
+                </div>
+                <div class="col-4 bg-success rounded">
+                    right
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+@endsection
