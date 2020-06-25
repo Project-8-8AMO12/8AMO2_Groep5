8aa82fa234337cf254949cda580a529067ec6440
Ilias Boujibar <1035133@mborijnland.nl>
2020-06-25 11:21:50 +0200

    zwermgezien afgemaakt
    de css en php pagina afgemaakt van zwermgezien

--- resources/views/frontend/zwermGezien.blade.php
+++ resources/views/frontend/zwermGezien.blade.php
@@ -26,8 +26,21 @@
         </div>
 
         <div class="mid_info">
-            <div class="foto_long">
-            <div class="foto_short">
+            <div class="foto_long">foto</div>
+            <div class="foto_short">foto 2</div>
+        </div>
+
+        <div class="bottom_info">
+            <div class="title"><h1>Wanneer contact opnemen?</h1></div>
+
+            <div class="content"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus, enim vitae sollicitudin 
+            euismod, mi sem ultricies nulla, ac auctor mauris risus sed lacus. Proin eget cursus lacus, eu ultricies est. Proin 
+            imperdiet est ac risus rhoncus imperdiet. Duis tincidunt pharetra justo, sed tincidunt quam rhoncus a. In faucibus 
+            ullamcorper dignissim. Suspendisse vel dui vitae massa rhoncus semper. Proin in commodo metus, vel faucibus purus. 
+            Fusce nec sollicitudin ante. Etiam ultricies, lorem sit amet fringilla tincidunt, orci nisl accumsan justo, 
+            ac maximus sapien odio quis nibh. Vivamus vel bibendum arcu. Curabitur consequat lobortis quam vitae malesuada.</p>
+            </div>
+        </div>
     </div>
 </div>
 
